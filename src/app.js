e8232c703575c2f1faf2a5bb76110abb9e9c0f69
Faizal <faizalamri15@gmail.com>
2024-08-08 08:03:01 +0700

    docs(swagger): add api documentation using swagger ui

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const routes = require("./utils/routes");
+const setupSwagger = require("./utils/swagger");
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+// set up swagger
+setupSwagger(app);
 
 // start server
 dotenv.config();
